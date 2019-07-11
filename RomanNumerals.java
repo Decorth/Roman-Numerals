f6bd315ab8e9f1f8017d8e1188dd33f702585236
Decorth <38436593+Decorth@users.noreply.github.com>
2019-07-11 21:11:53 +0100

    Decrease running time of roman numerals
    Using contains() to check if the string contains the exceptions and taking the difference away from the total is significantly faster than my previous solution.
    
    It has also decreased the memory usage of the application too!

--- RomanNumerals.java
+++ RomanNumerals.java
@@ -1,59 +1,33 @@
 // Map<Character, Integer> roman_map = new HashMap<Character, Integer>()
+// 
+// for (i = 0; i < s.length()) 
 //
-// for (i = 0; i < s.length())
-//
-// if (s.charAt(i) == I && s.charAt(i+1) == V) do this for 6 exceptions
-//
+// 
 // total = total + Map.get(s.charAt(i))
 // unless exception where total = total + 4
 //
 
 import java.util.HashMap;
 
-public int romanToInt(String s) {
-    int total = 0;
-    Map<Character, Integer> roman_map = new HashMap<Character, Integer>();
-    roman_map.put('I', 1);
-    roman_map.put('V', 5);
-    roman_map.put('X', 10);
-    roman_map.put('L', 50);
-    roman_map.put('C', 100);
-    roman_map.put('D', 500);
-    roman_map.put('M', 1000);
-    for (int i = 0; i < s.length(); i++){
-       try{
-            if (s.charAt(i) == 'I' && s.charAt(i+1) == 'V'){
-                total = total + 4;
-                i = i+1;
-            }
-            else if (s.charAt(i) == 'I' && s.charAt(i+1) == 'X'){
-                total = total + 9;
-                i = i+1;
-            }
-            else if (s.charAt(i) == 'X' && s.charAt(i+1) == 'L'){
-                total = total + 40;
-                i = i+1;
-            }
-            else if (s.charAt(i) == 'X' && s.charAt(i+1) == 'C'){
-                total = total + 90;
-                i = i+1;
-            }
-            else if (s.charAt(i) == 'C' && s.charAt(i+1) == 'D'){
-                total = total + 400;
-                i = i+1;
-                }
-            else if (s.charAt(i) == 'C' && s.charAt(i+1) == 'M'){
-                total = total + 900;
-                i = i+1;
-            }
-            else{
+class Solution {
+    public int romanToInt(String s) {
+        int total = 0;
+        Map<Character, Integer> roman_map = new HashMap<Character, Integer>();
+        roman_map.put('I', 1);
+        roman_map.put('V', 5);
+        roman_map.put('X', 10);
+        roman_map.put('L', 50);
+        roman_map.put('C', 100);
+        roman_map.put('D', 500);
+        roman_map.put('M', 1000);
+        for (int i = 0; i < s.length(); i++){
                 total = total + roman_map.get(s.charAt(i));
-            }
-
         }
-        catch (Exception e){
-          total = total + roman_map.get(s.charAt(i));
-        }
+        
+        if (s.contains("IV") || s.contains("IX")){total = total - 2;}
+        if (s.contains("XL") || s.contains("XC")){total = total - 20;}
+        if (s.contains("CM") || s.contains("CD")){total = total -200;}
+        
+        return total;
     }
-    return total;
 }
